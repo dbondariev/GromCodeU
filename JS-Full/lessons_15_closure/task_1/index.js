f4d02bd7752be4704d5115c240f2537996e4790d
dbondariev <dima.bondariev@gmail.com>
2022-07-27 11:35:05 +0300

    commit live coding

--- JS-Full/lessons_15_closure/task_1/index.js
+++ JS-Full/lessons_15_closure/task_1/index.js
@@ -1,13 +1,13 @@
 // задача создать счетчик (counter)
 
-
 // с помощью этой функции создавать счетчики
 export const makeCounter = () => {
-  let count = 0;
-  return function() {
-    return count++
-  }
-}
+ let count = 0;
+ return function () {
+  // eslint-disable-next-line no-plusplus
+  return count++;
+ };
+};
 
 const counter1 = makeCounter();
 const counter2 = makeCounter();
@@ -16,10 +16,10 @@ const counter2 = makeCounter();
 // нужно чтоб каждая функция отсчитывала от отдельно своей переменной
 // в момент выз ф-ции для нее созд свое лекс окружение и вызывается своя отдельная переменная
 
-console.log(counter1());// 0
-console.log(counter1());// 1
-console.log(counter1());// 2
+console.log(counter1()); // 0
+console.log(counter1()); // 1
+console.log(counter1()); // 2
 
-console.log(counter2());// 0
-console.log(counter2());// 1
-console.log(counter2());// 2
+console.log(counter2()); // 0
+console.log(counter2()); // 1
+console.log(counter2()); // 2
--- JS-Full/lessons_15_closure/task_2/index.js
+++ JS-Full/lessons_15_closure/task_2/index.js
@@ -1,27 +1,24 @@
 export const createCalculator = () => {
-  let memoryOfNumbers = 0;
+ let memoryOfNumbers = 0;
 
-  const add = number => {
-    memoryOfNumbers += number;
-    return memoryOfNumbers;
-  };
-  const decrease = number => {
-    memoryOfNumbers -= number;
-    return memoryOfNumbers;
-  };
-  const reset = () => {
-    memoryOfNumbers = 0;
-    return memoryOfNumbers;
-  };
-  const getMemo = () => memoryOfNumbers;
-  
-  return {
-    add,
-    decrease,
-    reset,
-    getMemo,
-  };
-}
+ const add = number => {
+  memoryOfNumbers += number;
+  return memoryOfNumbers;
+ };
+ const decrease = number => {
+  memoryOfNumbers -= number;
+  return memoryOfNumbers;
+ };
+ const reset = () => {
+  memoryOfNumbers = 0;
+  return memoryOfNumbers;
+ };
+ const getMemo = () => memoryOfNumbers;
 
-
-
+ return {
+  add,
+  decrease,
+  reset,
+  getMemo,
+ };
+};
