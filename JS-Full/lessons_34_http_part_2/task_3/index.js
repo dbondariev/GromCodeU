a605d052c3c825b34526f151854805fc3776b8bc
Dima Bondariev <86620950+dbondariev@users.noreply.github.com>
2022-08-19 17:47:23 +0300

    Update index.js

--- JS-Full/lessons_34_http_part_2/task_3/index.js
+++ JS-Full/lessons_34_http_part_2/task_3/index.js
@@ -1,57 +1,35 @@
 const loginForm = document.querySelector('.login-form');
 const submitBtn = document.querySelector('.submit-button');
-const email = document.querySelector('#email');
-const userName = document.querySelector('#name');
-const password = document.querySelector('#password');
-const error = document.querySelector('.error-text');
+const errorText = document.querySelector('.error-text');
 
-const baseUrl = 'https://6151c2184a5f22001701d3f3.mockapi.io/api/v1/users';
+const serverUrl = 'https://62d55c2c15ad24cbf2c55d15.mockapi.io/api/v1/users';
 
-const getUserData = () => fetch(baseUrl).then(response => response.json());
+const validateFormHandler = () => {
+ if (loginForm.reportValidity()) {
+  submitBtn.removeAttribute('disabled');
+ } else {
+  submitBtn.setAttribute('disabled', true);
+ }
+};
+loginForm.addEventListener('input', validateFormHandler);
 
-const saveUser = userData =>
-  fetch(baseUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(userData),
+const createUserHandler = event => {
+ event.preventDefault();
+
+ const user = Object.fromEntries(new FormData(loginForm));
+
+ fetch(serverUrl, {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(user),
+ })
+  .then(response => response.json())
+  .then(body => {
+   loginForm.reset();
+   alert(JSON.stringify(body));
+  })
+  .catch(() => {
+   errorText.textContent = 'Failed to create user';
   });
-
-const onSendUserData = event => {
-  event.preventDefault();
-
-  const userData = {
-    email: email.value,
-    name: userName.value,
-    password: password.value,
-  };
-
-  saveUser(userData)
-    .then(data => data.json())
-    .then(data => {
-      alert(JSON.stringify(data));
-      loginForm.reset();
-    })
-    .catch(() => {
-      error.textContent = 'Failed to create user';
-      return error;
-    });
 };
-
-const checkValidForm = () =>
-  loginForm.reportValidity()
-    ? submitBtn.removeAttribute('disabled', 'disabled')
-    : submitBtn.setAttribute('disabled', 'disabled');
-
-const cleanError = () => {
-  error.innerHTML = '';
-};
-
-submitBtn.addEventListener('click', onSendUserData);
-loginForm.addEventListener('input', checkValidForm);
-loginForm.addEventListener('input', cleanError);
-
-
-
-
+loginForm.addEventListener('submit', createUserHandler);
