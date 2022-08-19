a5d40409f2035d19e6cb1379531e508221af3722
dbondariev <dima.bondariev@gmail.com>
2022-08-19 16:18:11 +0300

    livecoding events and promises and fetch

--- JS-Full/lessons_34_http_part_2/task_2/index.js
+++ JS-Full/lessons_34_http_part_2/task_2/index.js
@@ -1,70 +1,96 @@
 const baseUrl = 'https://6151c2184a5f22001701d3f3.mockapi.io/api/v1/users';
 
 export function getUsersList() {
-  return fetch(baseUrl).then(response => response.json());
+ return fetch(baseUrl).then(response => response.json());
 }
 
 export function getUserById(userId) {
-  return fetch(`${baseUrl}/${userId}`).then(response => response.json());
+ return fetch(`${baseUrl}/${userId}`).then(response => response.json());
 }
 
 export function createUser(userData) {
-  return fetch(baseUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(userData),
-  });
+ return fetch(baseUrl, {
+  method: 'POST',
+  headers: {
+   'Content-Type': 'application/json;charset=utf-8',
+  },
+  body: JSON.stringify(userData),
+ });
 }
 
 export function deleteUser(userId) {
-  return fetch(`${baseUrl}/${userId}`, {
-    method: 'DELETE',
-  });
+ return fetch(`${baseUrl}/${userId}`, {
+  method: 'DELETE',
+ });
 }
 
 export function updateUser(userId, userData) {
-  return fetch(`${baseUrl}/${userId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(userData),
-  });
+ return fetch(`${baseUrl}/${userId}`, {
+  method: 'PUT',
+  headers: {
+   'Content-Type': 'application/json;charset=utf-8',
+  },
+  body: JSON.stringify(userData),
+ });
 }
 
-// examples
-// getUsersList().then(users => {
-//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-// });
+const formElem = document.querySelector('.login-form');
 
-// getUserById('2').then(userData => {
-//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-// });
+const serverUrl = 'https://6272c6bfa6522e24ac3e537b.mockapi.io/v1/users';
 
-// const newUserData = {
-//   email: 'cool@email.com',
-//   firstName: 'Iron',
-//   lastName: 'Man',
-//   age: 42,
-// };
+function createUserHandler(event) {
+ event.preventDefault();
+ // const formData = new FormData(formElem);
+ const user = {
+  email: 'zsn1p3@me.com',
+  name: 'Dima',
+  password: '12345',
+ };
+ fetch(serverUrl, {
+  method: 'POST',
+  header: {
+   'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(user),
+ })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+}
 
-// createUser(newUserData).then(() => {
-//   console.log('User created');
-// });
+formElem.addEventListener('submit', createUserHandler);
 
-// const updatedUserData = {
-//   email: 'new@email.com',
-//   firstName: 'John',
-//   lastName: 'Doe',
-//   age: 17,
-// };
+// examples;
+getUsersList().then(users => {
+ console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+});
 
-// updateUser('1', updatedUserData).then(() => {
-//   console.log('User updated');
-// });
+getUserById('2').then(userData => {
+ console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+});
 
-// deleteUser('2').then(() => {
-//   console.log('User updated');
-// });
+const newUserData = {
+ email: 'cool@email.com',
+ firstName: 'Iron',
+ lastName: 'Man',
+ age: 42,
+};
+
+createUser(newUserData).then(() => {
+ console.log('User created');
+});
+
+const updatedUserData = {
+ email: 'new@email.com',
+ firstName: 'John',
+ lastName: 'Doe',
+ age: 17,
+};
+
+updateUser('1', updatedUserData).then(() => {
+ console.log('User updated');
+});
+
+deleteUser('2').then(() => {
+ console.log('User updated');
+});
--- live_coding/events/index.js
+++ live_coding/events/index.js
@@ -1,37 +1,34 @@
-// EVENTS:
-// 1. types of events (top 5);
-// 2.add events(witch even type & on which elemet to add event);
-// 3. handlers(what to do when event is triggered);
+// events
+// 1. types of events (top-5)
+// 2. add events (which event type & on which element)
+// 3. handlers
 
-// WEB FLOWL:
-// 1.get data;
-// 2.render data;
-// 3.update data, don't update DOM!!!;
-// 4.render updated data;
-
-const listElem = document.querySelector('.list');
-const inputElem = document.querySelector('.task-input');
-const createBtn = document.querySelector('.create-task-btn');
-const checkboxElem = document.querySelector('.list__item-checkbox');
+// WEB FLOW
+// 1. get data
+// 2. render
+// 3. update data, don't update DOM (!!!!)
+// 4. re-render
 
 const tasks = [
- { id: Math.random().toString(), text: 'Buy milk', done: false },
- { id: Math.random().toString(), text: 'Pick up Tom from airport', done: false },
- { id: Math.random().toString(), text: 'Visit party', done: false },
- { id: Math.random().toString(), text: 'Visit doctor', done: true },
- { id: Math.random().toString(), text: 'Buy meat', done: true },
+ { text: 'Buy milk', done: false },
+ { text: 'Pick up Tom from airport', done: false },
+ { text: 'Visit party', done: false },
+ { text: 'Visit doctor', done: true },
+ { text: 'Buy meat', done: true },
 ];
 
+const listElem = document.querySelector('.list');
+
 const renderTasks = tasksList => {
- [...listElem.children].forEach(listItem => listItem.remove());
+ listElem.innerHTML = '';
+
  const tasksElems = tasksList
   .sort((a, b) => a.done - b.done)
-  .map(({ id, text, done }) => {
+  .map(({ text, done }) => {
    const listItemElem = document.createElement('li');
    listItemElem.classList.add('list__item');
    const checkbox = document.createElement('input');
    checkbox.setAttribute('type', 'checkbox');
-   checkbox.dataset.id = id;
    checkbox.checked = done;
    checkbox.classList.add('list__item-checkbox');
    if (done) {
@@ -45,38 +42,19 @@ const renderTasks = tasksList => {
  listElem.append(...tasksElems);
 };
 
-const createTask = () => {
- if (inputElem.value === '') {
+renderTasks(tasks);
+
+// ======
+const onToggleTask = e => {
+ const isCheckbox = e.target.classList.contains('list__item-checkbox');
+
+ if (!isCheckbox) {
   return;
  }
 
- tasks.push({ id: Math.random().toString(), text: inputElem.value, done: false });
- renderTasks(tasks);
- inputElem.value = '';
-};
-
-// const onToggleTask = e => {
-//  const isCheckbox = e.target.classList.contains('list__item-checkbox');
-
-//  if (!isCheckbox) {
-//   return;
-//  }
-
-//  const taskData = tasks.find(task => task.id === e.target.dataset.id);
-//  Object.assign(taskData, { done: e.target.checked });
-//  renderTasks(tasks);
-// };
-
-// listElem.addEventListener('click', onToggleTask);
-
-const markAsDone = event => {
- const targetElem = tasks.find(el => el.id === event.target.dataset.id);
- targetElem.done = !targetElem.done;
+ const taskData = tasks.find(task => task.id === e.target.dataset.id);
+ Object.assign(taskData, { done: e.target.checked });
  renderTasks(tasks);
 };
 
-createBtn.addEventListener('click', createTask);
-listElem.addEventListener('click', markAsDone);
-
-renderTasks(tasks);
-// Events:
+listElem.addEventListener('click', onToggleTask);
